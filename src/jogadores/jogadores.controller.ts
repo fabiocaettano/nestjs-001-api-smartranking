ffe3b4efbeced5c9baab075d150304167fcf7014
fabiocaettano <fabio.caettano74@gmail.com>
2022-11-08 01:07:04 +0000

    criado um metodo para testar o verb POST

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('api/v1/jogadores')
-export class JogadoresController {}
+export class JogadoresController {
+  
+    @Post()
+    async criarAtualizarJogador(){
+        return JSON.stringify({
+            "nome": "Fabio"
+        })	
+    }		
+}
